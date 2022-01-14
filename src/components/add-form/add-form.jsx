7ac4db4d2fe35c5d4bc3132e344fad72356c663c
Aleksandr Rozhok <akkelaforest@gmail.com>
2022-01-14 12:46:38 +0300

    Add event added star for employees and write method onChange for component AddFrom

--- src/components/add-form/add-form.jsx
+++ src/components/add-form/add-form.jsx
@@ -1,23 +1,50 @@
+import { Component } from 'react';
+
 import './add-form.css';
 
-const AddForm = () => {
-    return (
-        <div className="app-add-form">
-            <h3>Добавьте нового сотрудника</h3>
-            <form
-                className="add-form d-flex">
-                <input type="text"
-                    className="form-control new-post-label"
-                    placeholder="Как его зовут?" />
-                <input type="number"
-                    className="form-control new-post-label"
-                    placeholder="З/П в $?" />
+class AddForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: '',
+            salary: ''
+        }
+    }
 
-                <button type="submit"
-                    className="btn btn-outline-light">Добавить</button>
-            </form>
-        </div>
-    )
+    onInputChange = (e) => {
+        this.setState({
+            [e.target.name]: e.target.value
+        })
+    }
+
+    render() {
+
+        const { name, salary } = this.state;
+
+        return (
+            <div className="app-add-form">
+                <h3>Добавьте нового сотрудника</h3>
+                <form
+                    className="add-form d-flex">
+                    <input type="text"
+                        className="form-control new-post-label"
+                        onChange={this.onInputChange}
+                        name="name"
+                        value={name}
+                        placeholder="Как его зовут?" />
+                    <input type="number"
+                        className="form-control new-post-label"
+                        onChange={this.onInputChange}
+                        name="salary"
+                        value={salary}
+                        placeholder="З/П в $?" />
+
+                    <button type="submit"
+                        className="btn btn-outline-light">Добавить</button>
+                </form>
+            </div>
+        )
+    }
 }
 
 export default AddForm;
